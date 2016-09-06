021a9ca010f1af328f48953866d89a3b6f543a8e
qinting513 <513924632@qq.com>
2016-09-06 23:40:09 +0800

    在baseVC中利用extension隔离不同类别的函数

--- WeiBo/WeiBo/Classes/View[视图跟控制器]/Main/WBBaseViewController.swift
+++ WeiBo/WeiBo/Classes/View[视图跟控制器]/Main/WBBaseViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class WBBaseViewController: UIViewController {
+//面试题 OC中支持多继承吗 如果不支持，如何替代  答案： 使用协议替代
+//class WBBaseViewController: UIViewController ,UITableViewDataSource,UITableViewDelegate{
+//swift 中，利用‘extension’可以把函数按照功能分类管理，便于阅读跟维护
+//注意：
+//1. extension中不能有属性
+//2.extension中不能重写父类方法，重写父类方法，是子类的职责，扩展是对类的扩展，使类的功能更强大
 
+//主控制机器基类
+class WBBaseViewController: UIViewController ,UITableViewDataSource,UITableViewDelegate {
     //    如果用户没有登录 则不创建
     var tableView : UITableView?
     
@@ -42,10 +49,15 @@ extension WBBaseViewController {
         setupTableView()
     }
     
+//    MARK: - 设置tableView
     private func setupTableView(){
-    
+    tableView = UITableView(frame: view.bounds, style: .plain)
+        view.insertSubview(tableView!, belowSubview: navigationBar)
+        tableView?.dataSource = self
+        tableView?.delegate = self
     }
     
+//    MARK: - 设置导航条
     private func setupNavi(){
         //        添加一个导航栏
         view.addSubview(navigationBar)
@@ -59,4 +71,16 @@ extension WBBaseViewController {
         navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.darkGray()]
     }
 
+}
+
+extension WBBaseViewController {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+//    基类只是准备方法，子类负责具体实现，子类的数据源方法不需要super
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        只是保证没有语法错误
+        return UITableViewCell()
+    }
 }
