0eb10aa8536594a578c488b0469b248a973fb078
qinting513 <513924632@qq.com>
2016-09-07 19:17:26 +0800

    appDelegate中加载json

--- WeiBo_V1/WeiBo/AppDelegate.swift
+++ WeiBo_V1/WeiBo/AppDelegate.swift
@@ -22,10 +22,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = WBMainTabBarController()
         
         window?.makeKeyAndVisible()
-        
+        loadAppInfo()
         return true
     }
 
  
 }
 
+
+//MARK: - 从服务器加载应用程序信息
+extension AppDelegate{
+    private func loadAppInfo(){
+        DispatchQueue.main.async {
+            let  url = Bundle.main().pathForResource("main.json", ofType: nil)
+            let data = NSData(contentsOfFile: url!)
+            //写入磁盘
+            let docDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            let jsonPath = (docDir as NSString).appendingPathComponent("main.json")
+            data?.write(toFile: jsonPath, atomically: true)
+            print("应用程序加载完毕"+jsonPath)
+        }
+        
+    }
+    
+}
